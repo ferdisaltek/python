90384c484a5c500a4f497a80754de7000f734224
ferdisaltek <ferdisaltek@gmail.com>
2022-09-29 22:29:43 +0300

    func_try

---
+++ from unittest import FunctionTestCase.py
@@ -0,0 +1,10 @@
+def forman_name(f_name,l_name):
+    format_f_name=f_name.title()
+    format_l_name=l_name.title()
+    return f"{format_f_name} {format_l_name}"
+
+print(forman_name("ferdi","saltek"))
+
+
+
+
