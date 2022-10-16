7e824bd81798b8256607f4a630676519054ab46d
ferdisaltek <ferdisaltek@gmail.com>
2022-10-16 13:52:03 +0300

    commit more

--- functions.py
+++ functions.py
@@ -26,3 +26,35 @@ def selamla():
 
 
 print(sonuc)
+
+
+def selamla(isim):
+    return "Merhaba, " + isim
+
+sonuc = selamla("ferdi")
+sonuc = selamla("saltek")
+
+def toplam(a,b):
+    return a + b
+
+sonuc = toplam(10,20)
+sonuc = toplam(20,30)
+
+def yasHesapla(dogumYili):
+    return 2021 - dogumYili
+
+sonuc = yasHesapla(1983)
+sonuc = yasHesapla(1993)
+
+def emekliligeKacYilKaldi(dogumYili, isim):
+    yas = yasHesapla(dogumYili)
+
+    kalanSure = 65 - yas
+
+    if kalanSure > 0:
+        print(f"{isim}, emekliliğinize {kalanSure} yil kaldi.")
+    else:
+        print(f"{isim}, zaten {abs(kalanSure)} yil önce emekli oldunuz.")
+
+emekliligeKacYilKaldi(1983, "ali")
+emekliligeKacYilKaldi(1950, "veli")
