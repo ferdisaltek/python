7800a6180bd8194b47b364fb83158d5828d5978b
ferdisaltek <ferdisaltek@gmail.com>
2022-10-20 19:11:13 +0300

    commit

--- for_control.py
+++ for_control.py
@@ -2,9 +2,10 @@ def solution(numbers):
     sorted_list = sorted(numbers)
     list_len = len(sorted_list)
     for i in range(list_len - 2):
-        if sorted_list[i] > sorted_list[i + 2] - sorted_list[i + 1]:
-            return 1
-    
-    return 0
+        print(sorted_list[i])
+        print(sorted_list[i+2])
+        print(sorted_list[i+1])
+        print(i)
+     
 
 solution([10, 2, 5, 1, 8, 20])
