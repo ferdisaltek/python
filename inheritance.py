bc2a2452eeabe0f23ae826afef9e5f7449a5e595
ferdisaltek <ferdisaltek@gmail.com>
2022-11-22 23:02:25 +0300

    child_class

--- inheritance.py
+++ inheritance.py
@@ -12,14 +12,35 @@ class Person:  #base class ,parent class
 
 
 class student(Person):
-    pass
+    def __init__(self, name, lastname, age,number) -> None:
+        super().__init__(name, lastname, age)
+        #Person.__init__(self,name ,lastname,age)
+        self.number=number
+        print("student nesnesi oluşturuldu.")
+
+    def intro(self):
+        print(self.name, self.lastname, self.age, self.number)
+
+    def study(self):
+        print(f"{self.number} numaralı öğrenci ders çalıyor.")
 
 class teacher(Person):
-    pass
+    def __init__(self, name, lastname, age,branch) -> None:
+        super().__init__(name, lastname, age)
+        self.branch=branch
+
+    def teach(self):
+        print(f"{self.name} isimli öğretmen {self.branch} eğitimi veriyor.")
 
 
-p1=Person("ferdi","saltek",33)
-p1.intro()
+# p1=Person("ferdi","saltek",33)
+# p1.intro()
 
-s1=student("ali","veli",49)
+s1=student("ali","veli",49,550055)
 s1.intro()
+s1.study()
+
+t1=teacher("asdasd","qweqwe",44,"math")
+t1.intro
+print(t1.branch)
+t1.teach()
