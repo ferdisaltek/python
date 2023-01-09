9bf6d9c7c742aef41f1096f26815b25f572de47c
ferdisaltek <ferdisaltek@gmail.com>
2023-01-09 19:14:20 +0300

    g,thub demo selenium

--- github bot/app.py
+++ github bot/app.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from userinfo import username, password
 import time
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.common.by import By
 
 class Github:
-    
+
 
     def __init__(self):
         self.browser = webdriver.Chrome()
@@ -15,10 +15,12 @@ class Github:
 
     def signIn(self):
         self.browser.get(self.baseUrl + "login")
-        self.browser.find_element_by_name("login").send_keys(self.username)
-        self.browser.find_element_by_name("password").send_keys(self.password)
-        self.browser.find_element_by_name("commit").click()
-        
+        time.sleep(1)
+        self.browser.find_element(By.ID, 'login_field').send_keys(self.username)
+        self.browser.find_element(By.ID, "password").send_keys(self.password)
+        time.sleep(1)
+        self.browser.find_element(By.NAME, "commit").click()
+
 
     def __del__(self):
         time.sleep(4)
--- github bot/userinfo.py
+++ github bot/userinfo.py
@@ -1,2 +1,2 @@
 username="username"
-password=""
+password="121212"
