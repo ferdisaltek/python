47d019401d18927fad7a05cbac1d0b0fc3db8966
ferdisaltek <ferdisaltek@gmail.com>
2023-01-14 23:04:43 +0300

    instagram bot app

--- instagram/app.py
+++ instagram/app.py
@@ -1,24 +1,25 @@
 from selenium import webdriver
-from userinfo import username, password
+from userInfo import username, password
 from selenium.webdriver.common.keys import Keys
 import time
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-
 
 class Instagram:
 
- 
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.browser = webdriver.Chrome()
+        self.followers = []
+
+        self.browserProfile = webdriver.ChromeOptions()
+        self.browserProfile.add_experimental_option('prefs', {'intl.accept_languages':'en,en_US'})
+        self.browser = webdriver.Chrome(Instagram.driver_path, options=self.browserProfile)
 
     def signIn(self):
         self.browser.get("https://www.instagram.com/accounts/login/")
         time.sleep(3)
-        usernameInput = self.browser.find_element(By.NAME,'username')
-        passwordInput = self.browser.find_element(By.NAME,'password')
+        usernameInput = self.browser.find_element_by_name('username')
+        passwordInput = self.browser.find_element_by_name('password')
 
         usernameInput.send_keys(username)
         passwordInput.send_keys(password)
@@ -27,9 +28,9 @@ class Instagram:
 
         time.sleep(4)
 
-        if self.browser.find_element(By.CLASS_NAME, 'cmbtv'):
-            el = self.browser.find_element(By.CLASS_NAME,'cmbtv')
-            el.find_element(By.TAG_NAME,'button').click()
+        if self.browser.find_element_by_class_name('cmbtv'):
+            el = self.browser.find_element_by_class_name('cmbtv')
+            el.find_element_by_tag_name('button').click()
 
         time.sleep(4)
 
@@ -47,7 +48,7 @@ class Instagram:
 
         action = webdriver.ActionChains(self.browser)
 
-        print(f"takipçi sayisi: {count}")
+        print(f"takipçi sayısı: {count}")
 
         while count<max:
             modal.click()
@@ -65,7 +66,7 @@ class Instagram:
 
             if count != newCount:
                 count = newCount
-                print(f"takipçi sayisi: {count}")
+                print(f"takipçi sayısı: {count}")
                 time.sleep(1)
             else:
                 break
@@ -73,25 +74,66 @@ class Instagram:
         i = 0
         followers = modal.find_elements_by_tag_name("li")
         for user in followers:
-            i += 1
-            print(i)
             link = user.find_element_by_tag_name("a").get_attribute("href")
-            print(link)
+            self.followers.append(link)
+            i += 1
+            if i == max:
+                break
 
+        self.saveToFile(self.followers)
+
+    def saveToFile(self, followers):
+        with open("followers.txt", "w", encoding="UTF-8") as file:
+            for user in followers:
+                file.write(user + "\n")
 
     def followUser(self, username):
-        pass
+        self.browser.get(f"https://www.instagram.com/{username}/")
+        time.sleep(1)
+
+        followButton = self.browser.find_element_by_tag_name("button")
+
+        if followButton.text == "Follow" or followButton.text == "Follow":
+            followButton.click()
+            time.sleep(2)
+        else:
+            print(f"{username} sayfasını zaten takip ediyorsunuz.")
+
+    def followUsers(self, users):
+        for user in users:
+            self.followUser(user)
 
     def unFollowUser(self, username):
-        pass
+        self.browser.get(f"https://www.instagram.com/{username}/")
+
+        btn = self.browser.find_element_by_tag_name('button')
+
+        if btn.text == "Message":
+            self.browser.find_elements_by_tag_name('button')[1].click()
+            time.sleep(2)
+
+            self.browser.find_element_by_css_selector('div[role=dialog] button').click()
+        else:
+            print(f"{username} sayfasını zaten takip etmiyorsunuz.")
+
+    def unFollowUsers(self, users):
+        for user in users:
+            self.unFollowUser(user)
 
     def __del__(self):
         time.sleep(10)
-        # self.browser.close()
+        self.browser.close()
 
 app = Instagram(username, password)
 
-app.signIn()
-app.getFollowers()
-app.followUser('abc')
-app.unFollowUser('abc')
+app.signIn()    
+# app.followUser('user1')
+# app.followUser('user2')
+app.followUsers(["user1","user2"])
+
+# app.unFollowUser('user2')
+# app.unFollowUser('user1')
+
+app.unFollowUsers(["user1","user2"])
+
+app.getFollowers(50)
