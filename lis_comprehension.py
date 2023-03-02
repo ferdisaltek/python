437d90a474155511d861567b7e729950131bcbee
ferdisaltek <ferdisaltek@gmail.com>
2023-03-02 20:49:23 +0300

    list comprehension

---
+++ lis_comprehension.py
@@ -0,0 +1,9 @@
+# numbers=[1,1,2,3,5,8,13,21,34,55] 
+
+# new_list=[i*i for i in numbers]
+# print(new_list)
+
+
+numbers=[1,1,2,3,5,8,13,21,34,55] 
+even_list=[i for i in numbers if i%2==0 ]
+print(even_list)
