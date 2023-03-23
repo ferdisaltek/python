5902d58509c838059e1bc1692b3f630182264f14
ferdisaltek <ferdisaltek@gmail.com>
2023-03-23 23:23:31 +0300

    send email with python
    send email with python

---
+++ send_email/main.py
@@ -0,0 +1,14 @@
+import smtplib
+
+my_email="your_mail@gmail.com"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email,password="pass")
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="to_mailk@gmail.com",
+        msg="Subject:hello\n\n this is the body of my mail"
+        )
+
+
