578aeb80e9498968b3081004d49a4f968092d065
ferdisaltek <ferdisaltek@gmail.com>
2023-04-24 22:08:44 +0300

    api test
    api test

--- API/sunset.py
+++ API/sunset.py
@@ -2,7 +2,7 @@ from urllib import response
 import requests
 
 
-response=requests.get(url="https://api.sunrise-sunset.org/json?lat=41.035783&lng=29.225065&date=today")
+response=requests.get(url="https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today")
 print(response.status_code)
 response.raise_for_status()
 print(response.json())
