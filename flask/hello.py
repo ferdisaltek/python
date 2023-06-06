ceafd3f089f319d21092aa13c5e40efa3f248e5b
ferdisaltek <ferdisaltek@gmail.com>
2023-06-06 23:02:45 +0300

    more decorator and guess game

--- flask/hello.py
+++ flask/hello.py
@@ -6,8 +6,24 @@ def hello_world():
     return '<h1 style="text-align: center">Hello, World!</h1>'\
             '<p> this is  a paragraph.</p>'
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
 
 @app.route('/bye')
+@make_emphasis
 def say_bye():
     return 'bye bye'
 
